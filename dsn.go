5b7751d5321b1622170a67ca3ef49507b1f84ffd
虞翊 <steven.cheng@alibaba-inc.com>
2019-04-22 20:03:34 +0800

    support database name in connection url

--- dsn.go
+++ dsn.go
@@ -3,17 +3,19 @@ package gohive
 import (
 	"fmt"
 	"regexp"
+        "strings"
 )
 
 type Config struct {
 	User   string
 	Passwd string
 	Addr   string
+	DBName string
 }
 
 var (
 	// Regexp syntax: https://github.com/google/re2/wiki/Syntax
-	reDSN        = regexp.MustCompile(`(.+@)?([^@/]+)`)
+	reDSN        = regexp.MustCompile(`(.+@)?([^@]+)`)
 	reUserPasswd = regexp.MustCompile(`([^:@]+)(:[^:@]+)?@`)
 )
 
@@ -22,20 +24,32 @@ func ParseDSN(dsn string) (*Config, error) {
 	// Please read https://play.golang.org/p/_CSLvl1AxOX before code review.
 	sub := reDSN.FindStringSubmatch(dsn)
 	if len(sub) != 3 {
-		return nil, fmt.Errorf("The DSN %s doesn't match [user[:password]@]addr", dsn)
+		return nil, fmt.Errorf("The DSN %s doesn't match [user[:password]@]addr[/dbname]", dsn)
 	}
-
+	addr := ""
+	dbname := ""
+	loc := strings.IndexRune(sub[2], '/')
+        if loc > -1 {
+                addr = sub[2][:loc]
+                dbname = sub[2][loc+1:]
+        } else {
+                addr = sub[2]
+        }
 	up := reUserPasswd.FindStringSubmatch(sub[1])
 	if len(up) == 3 {
 		if len(up[2]) > 0 {
-			return &Config{User: up[1], Passwd: up[2][1:], Addr: sub[2]}, nil
+			return &Config{User: up[1], Passwd: up[2][1:], Addr: addr, DBName: dbname}, nil
 		}
-		return &Config{User: up[1], Addr: sub[2]}, nil
+		return &Config{User: up[1], Addr: addr, DBName: dbname}, nil
 	}
-	return &Config{Addr: sub[2]}, nil
+	return &Config{Addr: addr, DBName: dbname}, nil
 }
 
 // FormatDSN outputs a string in the format "user:password@address"
 func (cfg *Config) FormatDSN() string {
-	return fmt.Sprintf("%s:%s@%s", cfg.User, cfg.Passwd, cfg.Addr)
+        if len(cfg.DBName) > 0 {
+	        return fmt.Sprintf("%s:%s@%s/%s", cfg.User, cfg.Passwd, cfg.Addr, cfg.DBName)
+        } else {
+                return fmt.Sprintf("%s:%s@%s", cfg.User, cfg.Passwd, cfg.Addr)
+        }
 }
